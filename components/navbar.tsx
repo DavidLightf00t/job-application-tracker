f99a5b096ba899084c37dd0f1dd93199c2123ad2
David <dslightfoot123@gmail.com>
2026-02-18 02:20:37 -0500

    Update to show different navbar for signed in and signed out user and add an avatar if signed in

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,16 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuLabel, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu";
 import { Briefcase } from "lucide-react"
 import Link from "next/link";
+import SignOutButton from "@/components/sign-out-button";
+import { useSession } from "@/lib/auth/auth-client";
 
 export default function Navbar() {
+  const { data: session } = useSession();
+
   return (
     <nav className="border-b border-gray-200 bg-white">
         <div className="container mx-auto flex h-16 items-center px-4 justify-between">
@@ -11,13 +19,58 @@ export default function Navbar() {
                 Job Tracker
             </Link>
             <div className="flex item-center gap-4">
-            <Link href="/sign-in">
-                <Button variant="ghost" className="text-gray-700 hover:text-black">Log In</Button>
-            </Link>
-            <Link href="/sign-up">
-                <Button className="bg-primary hover:bg-primary/90">Start for free</Button>
-            </Link>
-            </div>
+
+            {session?.user ? (
+                <>
+                    <Link href="/dashboard">
+                        <Button
+                            variant="ghost"
+                            className="text-gray-700 hover:text-black"
+                        >
+                            Dashboard
+                        </Button>
+                    </Link>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger>
+                            <Button 
+                                variant="ghost" 
+                                className="relative h-8 w-8 rounded-full"
+                            >
+                                <Avatar className="h-8 w-8">
+                                    <AvatarFallback className="bg-primary text-white">
+                                        {session.user.name[0].toUpperCase()}
+                                    </AvatarFallback>
+                                </Avatar>
+                            </Button>
+                        </DropdownMenuTrigger>
+
+                        <DropdownMenuContent className="w-56" align="end">
+                            <DropdownMenuLabel className="font-normal">
+                                <div className="flex flex-col space-y-1">
+                                    <p className="text-sm font-medium leading-none">
+                                        {session.user.name ?? "Anonymous"}
+                                    </p>
+                                    <p className="text-xs leading-none text-muted-foreground">
+                                        {session.user.email}
+                                    </p>
+                                </div>
+                            </DropdownMenuLabel>
+                            <SignOutButton/>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </>
+            ) : (
+            <>
+                <Link href="/sign-in">
+                    <Button variant="ghost" className="text-gray-700 hover:text-black">Log In</Button>
+                </Link>
+                <Link href="/sign-up">
+                    <Button className="bg-primary hover:bg-primary/90">Start for free</Button>
+                </Link>
+            </>
+            )}
+
+           </div>
         </div>
     </nav>
     );
